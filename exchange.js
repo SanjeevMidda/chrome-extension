f9aaa701fa2ae06b04ee08593f9a23d9afb21577
Sanjeev Midda <s.midda1@gmail.com>
2026-02-01 16:34:10 +0000

    setting up logic for our app

---
+++ exchange.js
@@ -0,0 +1,4 @@
+const amount = document.getElementById("amount");
+const currenmcy = document.getElementById("currency");
+const convert = document.getElementById("convert");
+const result = document.getElementById("result");
